141eba3d08f249d8dfa1245d1c7742d81a2b9063
anup4747 <anupdtarwade@gmail.com>
2025-08-10 09:10:32 +0530

    used localstorage for volume

--- wsChess/client/src/context/volumeContext.tsx
+++ wsChess/client/src/context/volumeContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 import type { VolumeStateContextType } from "../types/types";
 
 const VolumeStateContext = createContext<VolumeStateContextType | undefined>(
@@ -18,17 +18,29 @@ export const useVolumeStateContext = () => {
 export const VolumeStateProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const [musicLevel, setMusicLevel] = useState(() => {
+    const savedMusicLevel = localStorage.getItem("musicLevel");
+    return savedMusicLevel !== null ? Number(savedMusicLevel) : 40;
+  });
+  const [soundLevel, setSoundLevel] = useState(() => {
+    const savedSoundLevel = localStorage.getItem("soundLevel");
+    return savedSoundLevel !== null ? Number(savedSoundLevel) : 50;
+  });
 
-    const [musicLevel, setMusicLevel] = useState(50);
-    const [soundLevel, setSoundLevel] = useState(65);
+  useEffect(() => {
+    localStorage.setItem("musicLevel", musicLevel.toString());
+  }, [musicLevel]);
 
+  useEffect(() => {
+    localStorage.setItem("soundLevel", soundLevel.toString());
+  }, [soundLevel]);
   return (
     <VolumeStateContext.Provider
       value={{
         musicLevel,
         soundLevel,
         setMusicLevel,
-        setSoundLevel
+        setSoundLevel,
       }}
     >
       {children}
