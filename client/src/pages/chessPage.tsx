1dafdece2e223bf491fb88507b9548903bef4a8a
anup4747 <anupdtarwade@gmail.com>
2025-10-19 08:32:57 +0530

    fixed reset game error

--- client/src/pages/chessPage.tsx
+++ client/src/pages/chessPage.tsx
@@ -8,7 +8,7 @@ import Controls from "../components/Control";
 import PlayerCard from "../components/playerCard";
 import { useGameModeContext } from "../context/gameModeContext";
 import { usePlayerNameContext } from "../context/playerName";
-import { useLocation } from "react-router-dom"; 
+import { useLocation } from "react-router-dom";
 import { usePoitnsContext } from "../context/pointsContext";
 
 gsap.registerPlugin(useGSAP);
@@ -19,12 +19,12 @@ const PlayChess: React.FC = () => {
   const { turn, resetGame } = useBoardStateContext();
   const playerDashBoard = useRef<HTMLDivElement | null>(null);
   const board = useRef<HTMLDivElement | null>(null);
-  const {setGameMode} = useGameModeContext();
-  const {player1Name, setPlayer1Name, player2Name, setPlayer2Name} = usePlayerNameContext();
+  const { setGameMode } = useGameModeContext();
+  const { player1Name, setPlayer1Name, player2Name, setPlayer2Name } = usePlayerNameContext();
   const location = useLocation();
-  const {blackPoints, whitePoints} = usePoitnsContext();
+  const { blackPoints, whitePoints } = usePoitnsContext();
 
- useEffect(() => {
+  useEffect(() => {
     if (location.pathname === "/playoffline") {
       setPlayer1Name("Player 1");
       setPlayer2Name("Player 2");
@@ -37,7 +37,7 @@ const PlayChess: React.FC = () => {
   }, [location.pathname, setPlayer1Name, setPlayer2Name, setGameMode]);
 
   useGSAP(() => {
-     gsap.fromTo(
+    gsap.fromTo(
       playerDashBoard.current,
       { opacity: 0, y: -40 },
       { opacity: 1, y: 0, duration: 1.7, delay: 3.5, ease: "sine" }
@@ -53,30 +53,31 @@ const PlayChess: React.FC = () => {
       { opacity: 1, y: 0, duration: 1.7, delay: 4.5, ease: "sine.in" }
     );
   });
+
   return (
     <section
       className={`h-screen flex flex-col justify-center ${themeClasses} `}
     >
       <div ref={playerDashBoard} className="flex justify-between mb-2 px-4 md:px-7 sm:px-12">
 
-          <PlayerCard
-            name={player1Name}
-            points={blackPoints} 
-            isTurn={turn === "black"}
-            />
+        <PlayerCard
+          name={player1Name}
+          points={blackPoints}
+          isTurn={turn === "black"}
+        />
 
-          <PlayerCard
-            name={player2Name} 
-            points={whitePoints}
-            isTurn={turn === "white"}
-          />
-      </div> 
+        <PlayerCard
+          name={player2Name}
+          points={whitePoints}
+          isTurn={turn === "white"}
+        />
+      </div>
       <div ref={board}>
 
-      <Chessboard />
+        <Chessboard />
       </div>
       <div ref={control} className="flex justify-center items-center px-4 md:px-8 sm:px-12">
-        <Controls resetGame={resetGame} turn={turn} />
+        <Controls resetGame={resetGame || (() => {})} turn={turn} />
       </div>
     </section>
   );
